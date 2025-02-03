3f0b0da09d354b3760c522b747876dc2ef19e18b
Рыжук Владислав <vlad.ryzhuk@yandex.ru>
2025-02-04 02:57:34 +0700

    complete product tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -11,29 +12,64 @@ class ProductTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
-    public function test_products_successful(): void
+    public function test_products_index_success(): void
     {
-        $response = $this->get(route('products.index'));
-        $response->assertOk();
+        $this->get(route('products.index'))->assertOk();
     }
 
-    public function test_products_fail(): void
+    public function test_products_index_fail(): void
     {
-        $response = $this->post(route('products.index'));
-        $response->assertFound();
+        $this->post(route('products.index'))->assertForbidden();
     }
 
-    public function test_products_store_successful(): void
+    public function test_products_show_success(): void
     {
-        $response = $this->post(
+        $product = Product::factory()->create();
+        $this->get(route('products.show', [$product->id]))->assertOk();
+    }
+
+    public function test_products_show_fail(): void
+    {
+        $this->get(route('products.show', [1]))->assertNotFound();
+    }
+
+    public function test_products_store_success(): void
+    {
+        $this->actingAs(
+            User::factory()->create(['role' => 'admin'])
+        )->post(
             route('products.store'),
-            Product::factory()->make()
-        );
-        $response->assertOk();
+            Product::factory()->make()->getAttributes()
+        )->assertOk();
     }
 
     public function test_products_store_fail(): void
     {
+        $this->post(
+            route('products.store'),
+            Product::factory()->make()->getAttributes()
+        )->assertForbidden();
+    }
 
+    public function test_products_update_success(): void
+    {
+        $product = Product::factory()->create();
+        $this->actingAs(
+            User::factory()->create(['role' => 'admin'])
+        )->patch(
+            route('products.update', [$product->id]),
+            ['price' => $product->price + 1]
+        )->assertOk();
+    }
+
+    public function test_products_update_fail(): void
+    {
+        $product = Product::factory()->create();
+        $this->actingAs(
+            User::factory()->create(['role' => 'admin'])
+        )->patch(
+            route('products.update', [$product->id]),
+            $product->only('title')
+        )->assertForbidden();
     }
 }
